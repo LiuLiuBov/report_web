a8822c99181ca3a5bfd142b8fb11e44d307a8585
Liubov Poliakova <liubov@forestradar.com>
2025-03-28 23:38:40 +0200

    added server

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const fs = require('fs');
+const app = express();
+const PORT = 3000;
+
+app.use(express.static('.'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+let comments = [];
+
+if (fs.existsSync('comments.json')) {
+    comments = JSON.parse(fs.readFileSync('comments.json'));
+}
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/comments', (req, res) => {
+    res.json(comments);
+});
+
+app.post('/comments', (req, res) => {
+    const comment = req.body.comment;
+    comments.push(comment);
+    fs.writeFileSync('comments.json', JSON.stringify(comments));
+    res.json({ success: true, comment });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
